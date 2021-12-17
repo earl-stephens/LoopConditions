9ab3c2a68a0d3bb953af119c2133fe49288cdae8
earl-stephens <stephensearl@hotmail.com>
2021-12-16 19:00:28 -0700

    Wrote test to check for proper use of loop.

--- src/application/App.java
+++ src/application/App.java
@@ -3,14 +3,22 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		
+
 		int counter = 0;
-		//while continues to run while the condition is true
-		while(counter < 10) {
+		// while continues to run while the condition is true
+		while (counter < 10) {
 			System.out.printf("Counter: %d\n", counter);
 			counter = counter + 1;
 		}
-
 	}
 
+	public String printHello(int endNumber) {
+		int startNumber = 0;
+		String outputString = "Hello";
+		while (startNumber < endNumber) {
+			//System.out.print("Hello");
+			outputString = outputString + outputString;
+		}
+		return outputString;
+	}
 }
